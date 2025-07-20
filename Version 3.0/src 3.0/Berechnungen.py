Fixed power calculation.

Version 3.0/src 3.0/Berechnungen.py
@@ -7,6 +7,9 @@ from Konvertierung import Konvertierung
 
 class Berechnungen:
 
+
+
+    @staticmethod
     def berechnePower_P_fABC( fABC : List[float], freqs : np.ndarray, psds_normiert : np.ndarray ):
     
         P_fABC : List[float] = [0.0, 0.0, 0.0]
@@ -15,19 +18,18 @@ class Berechnungen:
 
             for k in range( len(freqs) ):
                 if( fABC[i]-1 < freqs[k] < fABC[i]+1 ):
-                    closeFreqs_Indices.append(i)
+                    closeFreqs_Indices.append(k)
 
             sum : float = 0.0
             for cFI in closeFreqs_Indices:
                 sum += psds_normiert[cFI]
             mean = sum / len(closeFreqs_Indices)
-            P_fABC[i] = mean ! falsch
-        print(P_fABC)
-        return 
+            P_fABC[i] = mean #! falsch
+        return P_fABC
 
 
     @staticmethod
-    def bestimme_P_fLMR( freqComb : str, P_fABC : List[float] ):
+    def bestimme_P_fLMR( freqComb : str, P_fABC : np.ndarray ):
         return Konvertierung.get_P_fLMR( freqComb, P_fABC )
     
 
@@ -100,6 +102,7 @@ class Berechnungen:
         mittlere_psd_dB : float = 10*np.log10(mittlere_psd)
 
         return psds, psds_dB, freqs
+
     
 
 

Version 3.0/src 3.0/Konvertierung.py
@@ -4,7 +4,7 @@ from typing import List
 class Konvertierung:
 
     @staticmethod
-    def get_P_fLMR( freqComb : str, P_fABC : List[float] ):
+    def get_P_fLMR( freqComb : str, P_fABC : np.ndarray ):
 
         P_fA = P_fABC[0]
         P_fB = P_fABC[1]
@@ -20,7 +20,7 @@ class Konvertierung:
             "CAB" : [ P_fC, P_fA, P_fB ],
             "CBA" : [ P_fC, P_fB, P_fA ] 
         }
-        return P_fLMR_per_freqCond[ freqComb ]
+        return np.array( P_fLMR_per_freqCond[ freqComb ] )
     
 
     bool_targetPerCond : dict = {
@@ -35,3 +35,4 @@ class Konvertierung:
         "right"  : np.array( [1, 1, 0] ),
         "both"   : np.array( [0, 1, 0] )
     }
+

Version 3.0/src 3.0/Normierung.py
@@ -3,11 +3,12 @@ import numpy as np
 
 from RohBlock import RohBlock
 from Berechnungen import Berechnungen
+from HelpMethods import HelpMethods
 
 
 class Normierung:
 
-    NORMIERUNGS_DIVIDENT_PARTICIANT_0 = None
+    NORMIERUNGS_DIVIDENT_PARTICIANT_0 = 12.829149943758924
     NORMIERUNGS_DIVIDENT_PARTICIANT_1 = None
     NORMIERUNGS_DIVIDENT_PARTICIANT_2 = None
     NORMIERUNGS_DIVIDENT_PARTICIANT_3 = None
@@ -31,26 +32,22 @@ class Normierung:
         for blockNr in range(n_blocks):  # berechne Durchschnitt für Block                                                            
             rawBlock = RohBlock.erzeuge_gecroppteRaw_fuerBlock(rawFull, pathVMRK, blockLength, blockNr )
             psds, psds_dB, freqs = Berechnungen.get_psds( rawBlock, blockLength )
-            meanPSD = Normierung.berechneMean_Eintraege( psds_dB )
-            list_means += meanPSD
-
-        return Normierung.berechneMean_Eintraege( list_means )  #berechne Durchschnitt aller Blöcke
+            meanPSD = HelpMethods.berechneMean_Eintraege( psds_dB )
+            list_means.append(meanPSD)
+            
+            print(f"blockNr = {blockNr}")
+            print(f"list_means = {list_means}")
+            
+        return HelpMethods.berechneMean_Eintraege( list_means )  #berechne Durchschnitt aller Blöcke
 
 
     @staticmethod
     def normiere_psds_dB( psds_dB : np.ndarray, normierungsDivident : float ) -> np.ndarray:
         psds_dB_normiert : List[float] = []
         for e in psds_dB:
-            psds_normiert.append( e / normierungsDivident )  
-        psds_normiert = np.array( psds_normiert )
+            psds_dB_normiert.append( e / normierungsDivident )  
+        psds_dB_normiert = np.array( psds_dB_normiert )
         return psds_dB_normiert
 
 
-    @staticmethod
-    def berechneMean_Eintraege( arr : np.ndarray | List ):
-        sum = 0.0
-        for e in arr:
-            sum += e
-        mean = sum / float( len(arr) )
-        return mean
 